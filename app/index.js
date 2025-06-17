bb8537290611198aad5d298279c67272005e8120
hjuham <juhamatti.hurppu@gmail.com>
2025-06-17 14:27:41 +0300

    Add production docker-compose

--- app/index.js
+++ app/index.js
@@ -24,7 +24,7 @@ const User = require("./models/User");
 const cors = require("cors");
 //Initialize app
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.disable("etag");
